400826ef7a00698ec3dbab050bab0e6412804b0c
arasabbasi <aras.abbasi@googlemail.com>
2015-12-02 10:58:13 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -27,7 +27,7 @@
 
 <?php
 require 'scraperwiki.php';
-$endtime = time() + (60 * 60) * 6; //1h 
+$endtime = time() + (60 * 60) * 23; //23h 
 for ($id = 1; $id <= 300000; $id++) {
 	if ($endtime <= time())
 	{
